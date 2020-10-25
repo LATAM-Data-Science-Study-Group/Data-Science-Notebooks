1cf6633bbdb751d96b7a6db356305f08e250a803
JoelBuenrostro <joelbuenrostro@outlook.com>
2020-10-25 05:51:01 -0600

    Tutorial pandas
    Actualizacion tablas pivote

--- .virtual_documents/Notebooks/El modulo Pandas.ipynb.py
+++ .virtual_documents/Notebooks/El modulo Pandas.ipynb.py
@@ -1,7 +1,27 @@
 import pandas as pd
+import numpy as np
 
 
 df = pd.read_csv(r'../Datasets/Pokemon.csv')
 
 
 df.head(10)
+
+
+df.iloc[5]
+
+
+df.iloc[5, 1]
+
+
+df.loc[5, 'Type 1']
+
+
+pivote = pd.pivot_table(df, values=['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'], index='Type 1', aggfunc=np.mean)
+
+pivote
+
+
+legendarios = df[df['Legendary'] == True]
+
+legendarios
